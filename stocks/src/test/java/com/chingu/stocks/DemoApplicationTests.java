53e31c9b77e3e609a9c7c04a267376af2145f159
eolculnamo2 <rbertram8@gmail.com>
2019-06-08 21:10:44 -0500

    project setup

---
+++ stocks/src/test/java/com/chingu/stocks/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chingu.stocks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
