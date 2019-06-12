59eb9512e424c9cd46d359e960a760a4e15b1a2c
eolculnamo2 <rbertram8@gmail.com>
2019-06-11 22:04:14 -0500

    .

--- stocks/src/main/java/com/chingu/stocks/controller/MainController.java
+++ stocks/src/main/java/com/chingu/stocks/controller/MainController.java
@@ -1,18 +1,18 @@
 package com.chingu.stocks.controller;
 
-import com.chingu.stocks.dao.UserDAO;
-import com.chingu.stocks.entity.User;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.chingu.stocks.dao.UserDAO;
+import com.chingu.stocks.entity.User;
 
 @Controller
 public class MainController {
 
   @GetMapping("/")
   public String homePage() {
-    UserDAO userDao = new UserDAO();
 
+    UserDAO userDao = new UserDAO();
     User user = new User("test","testpass","display","emailtest",100,100);
     userDao.saveUser(user);
 
